8bf0db3c977ab101d63a379d4c692b03661b53a0
Gross3LIA5 <gro19823@spengergasse.at>
2020-06-15 16:11:33 +0200

    initCommit

--- src/application/SmsDialog.java
+++ src/application/SmsDialog.java
@@ -14,6 +14,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
 
+
 public class SmsDialog extends Stage
 {
 	private GridPane gridPane;
